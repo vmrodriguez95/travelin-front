a5a3d78ab070c0740300513284637fd6d758239e
Víctor Martín Rodríguez <victormartin@MacBook-Pro-de-Victor.local>
2026-01-29 15:12:31 +0100

    feat(layout): move foundations to it's folder

---
+++ src/foundations/icons/index.ts
@@ -0,0 +1,9 @@
+export const meta = {
+  name: 'Iconos',
+  icon: 'icons',
+  tag: 'icons',
+  description: 'Iconos del sistema de diseño.',
+  order: 3
+}
+
+export { default as Demo } from './icons.demo.astro'
---
+++ src/foundations/typography/index.ts
@@ -0,0 +1,9 @@
+export const meta = {
+  name: 'Tipografía',
+  icon: 'letter',
+  tag: 'typography',
+  description: 'Tipografía del sistema de diseño.',
+  order: 1,
+}
+
+export { default as Demo } from './typography.demo.astro'
