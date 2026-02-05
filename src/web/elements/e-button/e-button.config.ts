8b3252fd9b525f2df39f2f814d1c2954740ad3f3
Víctor Martín Rodríguez <victormartin@MacBook-Pro-de-Victor.local>
2026-02-05 18:00:34 +0100

    feat(project): reestructure folders and imporve desagn system

--- src/web/elements/e-button/e-button.config.ts
+++ src/web/elements/e-button/e-button.config.ts
@@ -1,18 +1,25 @@
 export const config = {
   props: {
     size: {
+      name: 'size',
       label: 'Tamaño',
       type: 'select',
       default: 'fit',
       options: ['fit', 'full']
     },
     disabled: {
-      label: 'Deshabilitado',
-      type: 'boolean',
+      name: 'disabled',
+      label: 'Deshabilitar botón',
+      type: 'checkbox',
       default: false
     }
   },
   slots: {
-    default: 'Click me'
+    default: {
+      name: 'default',
+      label: 'Texto del botón',
+      type: 'text',
+      default: 'Click me'
+    }
   }
 }
