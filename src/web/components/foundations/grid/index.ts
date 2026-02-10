1724435579a9821ac59a9a289d7d378b2cef1cf2
Víctor Martín Rodríguez <victormartin@MacBook-Pro-de-Victor.local>
2026-02-10 12:32:03 +0100

    feat(grid): new utility grid

---
+++ src/web/components/foundations/grid/index.ts
@@ -0,0 +1,9 @@
+export const meta = {
+  name: 'Grid',
+  icon: 'grid',
+  tag: 'grid',
+  description: 'Sistema de cuadrícula del sistema de diseño.',
+  order: 2
+}
+
+export { default as Demo } from './grid.demo.astro'
