aa846f5496fc4277ec839cc2fa10d52e53ea906d
Víctor Martín Rodríguez <victormartin@MacBook-Pro-de-Victor.local>
2026-03-05 15:56:05 +0100

    refcator(e-header): change to c-header

---
+++ src/design-system/components/c-header/index.ts
@@ -0,0 +1,9 @@
+export const meta = {
+  name: 'Header',
+  icon: 'rectangle',
+  tag: 'c-header',
+  description: 'Header con o sin imagen de fondo'
+}
+
+export { config } from './c-header.config'
+export { default as Demo } from './c-header.demo.astro'
--- src/design-system/elements/e-header/index.ts
+++
@@ -1,9 +0,0 @@
-export const meta = {
-  name: 'Header',
-  icon: 'rectangle',
-  tag: 'e-header',
-  description: 'Header con o sin imagen de fondo'
-}
-
-export { config } from './e-header.config'
-export { default as Demo } from './e-header.demo.astro'
