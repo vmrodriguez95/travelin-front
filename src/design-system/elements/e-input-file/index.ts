0d00f792eb0a5e0f0b0986f1a4b3397f68ee8fc5
Víctor Martín Rodríguez <victormartin@MacBook-Pro-de-Victor.local>
2026-03-30 17:18:49 +0200

    feat(e-input-file): create new component

---
+++ src/design-system/elements/e-input-file/index.ts
@@ -0,0 +1,9 @@
+export const meta = {
+  name: 'Input file',
+  icon: 'attach',
+  tag: 'e-input-file',
+  description: 'Campo para adjuntar archivos del formulario'
+}
+
+export { config } from './e-input-file.config'
+export { default as Demo } from './e-input-file.demo.astro'
