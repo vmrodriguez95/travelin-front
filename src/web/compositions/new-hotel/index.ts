839d1268b15217aa5f05043b969970b6bfcc1692
Víctor Martín Rodríguez <victormartin@MacBook-Pro-de-Victor.local>
2026-03-30 17:20:27 +0200

    feat(new-hotel): new composition to create poi_hotel

---
+++ src/web/compositions/new-hotel/index.ts
@@ -0,0 +1,10 @@
+export const meta = {
+  name: 'Nuevo alojamiento',
+  icon: 'form',
+  tag: 'new-hotel',
+  description: 'Vista de creación de un nuevo alojamiento.',
+  order: 0
+}
+
+export { config } from './new-hotel.config'
+export { default as Demo } from './NewHotel.astro'
---
+++ src/web/compositions/new-hotel/new-hotel.config.ts
@@ -0,0 +1,12 @@
+import { getFormBy } from '@web/utils/form.utils'
+
+export const config = {
+  props: {
+    data: {
+      name: 'data',
+      label: 'Datos del formulario',
+      type: 'text',
+      default: getFormBy('poi_hotel', 'create')
+    },
+  }
+}
