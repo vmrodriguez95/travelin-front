cbea535956161c7da5b13c8d682ea4fceebc04fb
Víctor Martín Rodríguez <victormartin@MacBook-Pro-de-Victor.local>
2026-04-06 11:58:14 +0200

    feat(poi_notes): new form

---
+++ src/web/compositions/new-notes/index.ts
@@ -0,0 +1,10 @@
+export const meta = {
+  name: 'Nueva nota',
+  icon: 'form',
+  tag: 'new-notes',
+  description: 'Vista de creación de una nueva nota.',
+  order: 0
+}
+
+export { config } from './new-notes.config'
+export { default as Demo } from './NewNotes.astro'
