2ca8cbed50944f7b0e6a28f3e62aab2fea7b743d
Victor Martin Rodriguez <victor.martin@MADG-124821.madrid.unir.local>
2026-05-07 16:50:26 +0200

    feat(register): new view

---
+++ src/web/compositions/register/index.ts
@@ -0,0 +1,10 @@
+export const meta = {
+  name: 'Registro',
+  icon: 'person',
+  tag: 'register',
+  description: 'Vista de registro',
+  order: 0
+}
+
+export { config } from './register.config'
+export { default as Demo } from './Register.astro'
