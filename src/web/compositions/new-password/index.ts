eca6b36fa483a041bae6d0a1ad152b9333ead051
Victor Martin Rodriguez <victor.martin@MADG-124821.madrid.unir.local>
2026-05-11 16:00:22 +0200

    feat(reset-password): new views to reset users passwords

---
+++ src/web/compositions/new-password/index.ts
@@ -0,0 +1,10 @@
+export const meta = {
+  name: 'Crear nueva contraseña',
+  icon: 'eye',
+  tag: 'new-password',
+  description: 'Vista para recuperar la contraseña',
+  order: 0
+}
+
+export { config } from './new-password.config'
+export { default as Demo } from './NewPassword.astro'
