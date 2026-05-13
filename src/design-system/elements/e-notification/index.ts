fd057f755e9b6fb48cc6fd35b86be4ac11e74222
Victor Martin Rodriguez <victor.martin@MADG-124821.local>
2026-05-13 08:47:15 +0200

    feat(notification): new component

---
+++ src/design-system/elements/e-notification/index.ts
@@ -0,0 +1,9 @@
+export const meta = {
+  name: 'Notification',
+  icon: 'rectangle',
+  tag: 'e-notification',
+  description: 'Notificación flotante principal del sistema'
+}
+
+export { config } from './e-notification.config'
+export { default as Demo } from './e-notification.demo.astro'
