cc44073d54f0aa4224b91420a336219dc0101dfe
Victor Martin Rodriguez <victor.martin@MADG-124821.local>
2026-06-01 12:54:07 +0200

    feat(home): adjunst spaces

--- src/web/compositions/home/home.config.ts
+++ src/web/compositions/home/home.config.ts
@@ -1,12 +1,3 @@
-import { getFormBy } from '@web/utils/form.utils'
-
 export const config = {
-  props: {
-    data: {
-      name: 'data',
-      label: 'Datos del formulario',
-      type: 'text',
-      default: getFormBy('login', 'delete')
-    },
-  }
+  props: {}
 }
