3ca39e5a2c609aaee3cecdd998d186c4c4065cec
Victor Martin Rodriguez <victor.martin@MADG-124821.local>
2026-06-01 13:57:59 +0200

    feat(profile): new view

---
+++ src/web/compositions/profile/index.ts
@@ -0,0 +1,10 @@
+export const meta = {
+  name: 'Perfil',
+  icon: 'person',
+  tag: 'profile',
+  description: 'Vista del perfil',
+  order: 3
+}
+
+export { config } from './profile.config'
+export { default as Demo } from './Profile.astro'
